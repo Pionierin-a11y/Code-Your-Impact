a5f1ef787b1fc031fcbcb40732c1ca7726cb942a
Pionierin-a11y <Pionierin@uni-koblenz.de>
2026-06-23 12:33:38 +0200

    Update myscript.js

--- Amelie/myscript.js
+++ Amelie/myscript.js
@@ -1,1 +1,16 @@
+AFRAME.registerComponent('ar-interaktion', {
+        init: function () {
+          // Das Element (unser Video), auf das geklickt wird
+          let meinVideo = document.querySelector('#animation');
+          let meinSound = document.querySelector('#soundeffekt');
 
+          this.el.addEventListener('click', () => {
+            // Video abspielen (falls es pausiert war)
+            meinVideo.play();
+            
+            // Sound abspielen
+            meinSound.currentTime = 0; // Sound von vorne starten
+            meinSound.play();
+          });
+        }
+      });
